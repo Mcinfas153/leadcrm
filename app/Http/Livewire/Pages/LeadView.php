30a15769307ddf7d1a0dc7e5105d49421fb718c4
Mc Infas <mcinfas9394@gmail.com>
2023-08-04 14:17:44 +0400

    update lead view done

--- app/Http/Livewire/Pages/LeadView.php
+++ app/Http/Livewire/Pages/LeadView.php
@@ -4,25 +4,190 @@ namespace App\Http\Livewire\Pages;
 
 use App\Models\Lead;
 use App\Models\LeadStatus;
+use App\Models\Priority;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
 class LeadView extends Component
 {
-    public $lead;
+    public $leadId;
+    public $fullname;
+    public $phone;
+    public $secondaryPhone;
+    public $email;
+    public $whatsapp;
+    public $city;
+    public $country;
+    public $budget;
+    public $contactTime;
+    public $purpose;
+    public $inquiry;
+    public $campaignName;
+    public $propertyType;
+    public $bedroom;
+    public $status;
+    public $source;
+    public $priority;
+    public $developer;
+    public $type;
+    public $attachment;
+    public $assignTo;
+
+
+    public function mount($leadId)
+    {
+        $this->leadId = $leadId;
+        $lead = Lead::find($this->leadId);
+        $this->fullname = $lead->fullname;
+        $this->phone = $lead->phone;
+        $this->secondaryPhone = $lead->secondary_phone;
+        $this->email = $lead->email;
+        $this->whatsapp = $lead->whatsapp;
+        $this->city = $lead->city;
+        $this->country = $lead->country;
+        $this->budget = $lead->budget;
+        $this->contactTime = $lead->contact_time;
+        $this->purpose = $lead->purpose;
+        $this->inquiry = $lead->inquiry;
+        $this->campaignName = $lead->campaign_name;
+        $this->propertyType = $lead->property_type;
+        $this->bedroom = $lead->bedroom;
+        $this->status = $lead->status;
+        $this->source = $lead->source;
+        $this->priority = $lead->priority;
+        $this->developer = $lead->developer;
+        $this->type = $lead->type;
+        $this->attachment = $lead->attachment;
+        $this->assignTo = $lead->assign_to;
+    }
 
     public function render()
     {
         return view('livewire.pages.lead-view', [
-            'status' => LeadStatus::all(),
-            'users' => User::all(),
+            'statusList' => LeadStatus::where('is_active', 1)->get(),
+            'usersList' => User::where('business_id', Auth::user()->business_id)->get(),
+            'priorityList' => Priority::where('is_active', 1)->get(),
         ])->layout('layouts.app', [
             'title' => 'lead view',
         ]);
     }
 
-    public function mount($leadId)
+    public function updatedStatus($value)
     {
-        $this->lead = Lead::find($leadId);
+
+        DB::beginTransaction();
+
+        try {
+            
+            $lead = Lead::find($this->leadId);
+
+            $lead->status = $value;
+
+            $lead->save();
+
+            DB::commit();
+
+            $this->dispatchBrowserEvent('pushToast', ['icon' => 'success', 'title' => config('message.LEAD_STATUS_CHANGE_SUCCESS')]);
+
+        } catch (\Exception $e) {
+
+            DB::rollBack();
+
+            $this->dispatchBrowserEvent('pushToast', ['icon' => 'error', 'title' => config('message.SOMETHING_HAPPENED')]);
+
+        }
+    }
+
+    public function updatedAssignTo($value)
+    {
+
+        DB::beginTransaction();
+
+        try {
+            
+            $lead = Lead::find($this->leadId);
+
+            $lead->assign_to = $value;
+
+            $lead->save();
+
+            DB::commit();
+
+            $this->dispatchBrowserEvent('pushToast', ['icon' => 'success', 'title' => config('message.USER_ASSIGN_CHANGE_SUCCESS')]);
+
+        } catch (\Exception $e) {
+
+            DB::rollBack();
+
+            $this->dispatchBrowserEvent('pushToast', ['icon' => 'error', 'title' => config('message.SOMETHING_HAPPENED')]);
+
+        }
+    }
+
+    public function updatedPriority($value)
+    {
+
+        DB::beginTransaction();
+
+        try {
+            
+            $lead = Lead::find($this->leadId);
+
+            $lead->priority = $value;
+
+            $lead->save();
+
+            DB::commit();
+
+            $this->dispatchBrowserEvent('pushToast', ['icon' => 'success', 'title' => config('message.LEAD_PRIORITY_CHANGE_SUCCESS')]);
+
+        } catch (\Exception $e) {
+
+            DB::rollBack();
+
+            $this->dispatchBrowserEvent('pushToast', ['icon' => 'error', 'title' => config('message.SOMETHING_HAPPENED')]);
+
+        }
+    }
+
+    public function updateLead()
+    {
+        DB::beginTransaction();
+
+        try {
+            
+           Lead::find($this->leadId)
+            ->update([
+                'fullname' => $this->fullname,
+                'email' => $this->email,
+                'phone' => $this->phone,
+                'secondary_phone' => $this->secondaryPhone,
+                'whatsapp' => $this->whatsapp,
+                'city' => $this->city,
+                'country' => $this->country,
+                'budget' => $this->budget,
+                'contact_time' => $this->contactTime,
+                'purpose' => $this->purpose,
+                'inquiry' => $this->inquiry,
+                'campaign_name' => $this->campaignName,
+                'property_type' => $this->propertyType,
+                'bedroom' => $this->bedroom,
+                'source' => $this->source,
+                'developer' => $this->developer
+            ]);
+
+            DB::commit();
+
+            $this->dispatchBrowserEvent('pushToast', ['icon' => 'success', 'title' => config('message.LEAD_UPDATED_SUCCESS')]);
+
+        } catch (\Exception $e) {
+
+            DB::rollBack();
+
+            $this->dispatchBrowserEvent('pushToast', ['icon' => 'error', 'title' => config('message.SOMETHING_HAPPENED')]);
+
+        }
     }
 }
--- resources/views/livewire/pages/lead-view.blade.php
+++ resources/views/livewire/pages/lead-view.blade.php
@@ -2,6 +2,9 @@
     <livewire:components.navigator title="Lead Details"/>
 
     <div class="row">
+        <div wire:loading wire:target="status,assignTo,updateLead,priority">
+            <livewire:components.progress-loader/>
+        </div>
         <div class="">
             <div class="card">
                 <div class="card-header text-center bg-info">
@@ -12,31 +15,31 @@
                         <div class="row">
                             <div class="form-group mb-3 col-md-6">
                                 <small id="fullname" class="form-text text-muted">Full Name *</small>
-                                <input type="text" class="form-control mt-1"aria-describedby="fullname" placeholder="Full Name *" value="{{ $lead->fullname }}">
+                                <input type="text" class="form-control mt-1" wire:model.defer="fullname">
                             </div>
                             <div class="form-group mb-3 col-md-6">
                                 <small id="email" class="form-text text-muted">Email Address *</small>
-                                <input type="email" class="form-control mt-1"aria-describedby="email" placeholder="Email Address *" value="{{ $lead->email }}">
+                                <input type="email" class="form-control mt-1"aria-describedby="email" placeholder="Email Address *" wire:model.defer="email">
                             </div>
                             <div class="form-group mb-3 col-md-4">
                                 <small id="phone" class="form-text text-muted">Phone Number *</small>
-                                <input type="text" class="form-control mt-1"aria-describedby="phone" placeholder="Phone Number *" value="{{ $lead->phone }}">
+                                <input type="text" class="form-control mt-1"aria-describedby="phone" placeholder="Phone Number *" wire:model.defer="phone">
                             </div>
                             <div class="form-group mb-3 col-md-4">
                                 <small id="secondaryphone" class="form-text text-muted">Secondary Phone</small>
-                                <input type="text" class="form-control mt-1" aria-describedby="secondaryphone" placeholder="Secondary Phone" value="{{ $lead->secondary_phone }}">
+                                <input type="text" class="form-control mt-1" aria-describedby="secondaryphone" placeholder="Secondary Phone" wire:model.defer="secondaryPhone">
                             </div>
                             <div class="form-group mb-3 col-md-4">
                                 <small id="whatsapp" class="form-text text-muted">Whatsapp Number</small>
-                                <input type="text" class="form-control mt-1" aria-describedby="whatsapp" placeholder="Whatsapp Phone" value="{{ $lead->whatsapp }}">
+                                <input type="text" class="form-control mt-1" aria-describedby="whatsapp" placeholder="Whatsapp Phone" wire:model.defer="whatsapp">
                             </div>
                             <div class="form-group mb-3 col-md-6">
                                 <small id="city" class="form-text text-muted">City</small>
-                                <input type="text" class="form-control mt-1" aria-describedby="city" placeholder="City" value="{{ $lead->city }}">
+                                <input type="text" class="form-control mt-1" aria-describedby="city" placeholder="City" wire:model.defer="city">
                             </div>
                             <div class="form-group mb-3 col-md-6">
                                 <small id="country" class="form-text text-muted">Country</small>
-                                <input type="text" class="form-control mt-1" aria-describedby="country" placeholder="Country" value="{{ $lead->country }}">
+                                <input type="text" class="form-control mt-1" aria-describedby="country" placeholder="Country" wire:model.defer="country">
                             </div>
                         </div>
                     </form>
@@ -46,65 +49,69 @@
                     <h5 class="card-title text-light"><i class="ti ti-info-circle me-1 fs-6"></i> More Informations</h5>
                 </div>
                 <div class="card-body">
-                    <form>
+                    <form wire:submit.prevent="updateLead">
                         <div class="row">
                             <div class="form-group mb-3 col-md-6">
                                 <small id="campaign" class="form-text text-muted">Campaign Name</small>
-                                <input type="text" class="form-control mt-1"aria-describedby="campaign" placeholder="Campaign Name" value="{{ Str::title($lead->campaign_name) }}">
+                                <input type="text" class="form-control mt-1"aria-describedby="campaign" placeholder="Campaign Name" wire:model.defer="campaignName">
                             </div>
                             <div class="form-group mb-3 col-md-6">
                                 <small id="purpose" class="form-text text-muted">Purpose</small>
-                                <input type="text" class="form-control mt-1"aria-describedby="purpose" placeholder="Purpose" value="{{ Str::title($lead->purpose) }}">
+                                <input type="text" class="form-control mt-1"aria-describedby="purpose" placeholder="Purpose" wire:model.defer="purpose">
                             </div>
                             <div class="form-group mb-3 col-md-4">
                                 <small id="budget" class="form-text text-muted">Budget</small>
-                                <input type="text" class="form-control mt-1" aria-describedby="budget" placeholder="Budget" value="{{ $lead->budget }}">
+                                <input type="text" class="form-control mt-1" aria-describedby="budget" placeholder="Budget" wire:model.defer="budget">
+                            </div>                           
+                            <div class="form-group mb-3 col-md-4">
+                                <small id="propertytype" class="form-text text-muted">Property Type</small>
+                                <input type="text" class="form-control mt-1" aria-describedby="propertytype" placeholder="Property Type" wire:model.defer="propertyType">
                             </div>
                             <div class="form-group mb-3 col-md-4">
                                 <small id="bedroom" class="form-text text-muted">Bedrooms</small>
-                                <input type="text" class="form-control mt-1" aria-describedby="bedroom" placeholder="Bedrooms" value="{{ $lead->bedroom }}">
+                                <input type="text" class="form-control mt-1" aria-describedby="bedroom" placeholder="Bedrooms" wire:model.defer="bedroom">
                             </div>
                             <div class="form-group mb-3 col-md-4">
-                                <small id="contacttime" class="form-text text-muted">Preffered Time</small>
-                                <input type="text" class="form-control mt-1" aria-describedby="contacttime" placeholder="Preffered Time" value="{{ $lead->contact_time }}">
-                            </div>
-                            <div class="form-group mb-3 col-md-6">
                                 <small id="developer" class="form-text text-muted">Developer</small>
-                                <input type="text" class="form-control mt-1" aria-describedby="developer" placeholder="Developer" value="{{ Str::title($lead->developer) }}">
+                                <input type="text" class="form-control mt-1" aria-describedby="developer" placeholder="Developer" wire:model.defer="developer">
                             </div>
-                            <div class="form-group mb-3 col-md-6">
-                                <small id="propertytype" class="form-text text-muted">Property Type</small>
-                                <input type="text" class="form-control mt-1" aria-describedby="propertytype" placeholder="Property Type" value="{{ Str::title($lead->property_type) }}">
+                            <div class="form-group mb-3 col-md-4">
+                                <small id="contacttime" class="form-text text-muted">Preffered Call Time</small>
+                                <input type="text" class="form-control mt-1" aria-describedby="contacttime" placeholder="Preffered Time" wire:model.defer="contactTime">
                             </div>
-                            <div class="form-group mb-3 col-md-3">
+                            <div class="form-group mb-3 col-md-4">
                                 <small id="source" class="form-text text-muted">Source</small>
-                                <input type="text" class="form-control mt-1" aria-describedby="source" placeholder="Source" value="{{ Str::title($lead->source) }}">
+                                <input type="text" class="form-control mt-1" aria-describedby="source" placeholder="Source" wire:model.defer="source">
                             </div>
-                            <div class="form-group mb-3 col-md-3">
+                            <div class="form-group mb-3 col-md-4">
                                 <small id="status" class="form-text text-muted">Status</small>
-                                <select class="form-select mr-sm-2 mt-1" id="inlineFormCustomSelect">
-                                    <option selected disabled>Select a Status Value</option>
-                                    @foreach ($status as $st)
-                                        <option value="{{ $st->id }}">{{ $st->name }}</option>
+                                <select class="form-select mr-sm-2 mt-1" id="inlineFormCustomSelect" wire:model="status">
+                                    @foreach ($statusList as $sl)
+                                        <option value="{{ $sl->id }}">{{ $sl->name }}</option>
                                     @endforeach
                                 </select>
                             </div>
-                            <div class="form-group mb-3 col-md-6">
+                            <div class="form-group mb-3 col-md-4">
+                                <small id="bedroom" class="form-text text-muted">Priority</small>
+                                <select class="form-select mr-sm-2 mt-1" id="inlineFormCustomSelect" wire:model="priority">
+                                    @foreach ($priorityList as $pl)
+                                        <option value="{{ $pl->id }}">{{ $pl->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group mb-3 col-md-4">
                                 <small id="bedroom" class="form-text text-muted">Assigned To</small>
-                                <select class="form-select mr-sm-2 mt-1" id="inlineFormCustomSelect">
-                                    @foreach ($users as $user)
-                                        <option value="{{ $user->id }}">{{ $user->name }}</option>
+                                <select class="form-select mr-sm-2 mt-1" id="inlineFormCustomSelect" wire:model="assignTo">
+                                    @foreach ($usersList as $ul)
+                                        <option value="{{ $ul->id }}">{{ $ul->name }}</option>
                                     @endforeach
                                 </select>
                             </div>
                             <div class="form-group mb-3 col-md-12">
                                 <small id="inquiry" class="form-text text-muted">Inquiry</small>
-                                <textarea class="form-control mt-1"aria-describedby="inquiry" rows="4">{{ $lead->inquiry }}</textarea>
+                                <textarea class="form-control mt-1"aria-describedby="inquiry" rows="4" wire:model.defer="inquiry"></textarea>
                             </div>
-                            <div class="form-group mb-3 col-md-12">
-                                <small id="note" class="form-text text-muted">Note:</small>
-                                <textarea class="form-control mt-1"aria-describedby="note" rows="4">Write Something...</textarea>
-                            </div>
+                            
                             <div class="col-12">
                                 <div class="d-md-flex align-items-center mt-3">
                                     <div class="ms-auto mt-3 mt-md-0">
