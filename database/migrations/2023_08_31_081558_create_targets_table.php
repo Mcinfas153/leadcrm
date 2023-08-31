0e82704aac5215cc6d1191aedd455f244edca433
Mc Infas <mcinfas9394@gmail.com>
2023-08-31 12:21:51 +0400

    targets table, model added

---
+++ database/migrations/2023_08_31_081558_create_targets_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('targets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class);
+            $table->date('starting_date');
+            $table->date('ending_date');
+            $table->integer('total_amount');
+            $table->integer('achieved_amount');
+            $table->integer('business_id')->constrained('organizations');
+            $table->integer('created_by')->constrained('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('targets');
+    }
+};
