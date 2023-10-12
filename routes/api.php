2599b39a111addeaf64d7b1dc1c7ed8f4599ca9c
Mc Infas <mcinfas9394@gmail.com>
2023-10-12 13:47:14 +0400

    api url change, and mail fucntion on

--- routes/api.php
+++ routes/api.php
@@ -20,5 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::prefix('v1')->group(function () {
-    Route::get('/lead/create', [LeadController::class, 'create'])->name('lead.insert');
+    Route::post('/lead/create', [LeadController::class, 'create'])->name('lead.insert');
 });
